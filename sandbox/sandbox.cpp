e80d722107589b4d862d131828ecede1d8a38513
Martin Ertsås <mertsas@cisco.com>
2022-02-07 12:48:16 +0100

    Create a user namespace

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <fstream>
 
+#include <unistd.h>
 #include <dlfcn.h>
 
 namespace
 {
+
+int (*actual_main)(int, char**, char**) = nullptr;
 
 template<typename F>
 F dlsym_next(const char * symbol)
@@ -11,19 +15,36 @@ F dlsym_next(const char * symbol)
     return reinterpret_cast<F>(dlsym(RTLD_NEXT, symbol));
 }
 
+void setgroups_deny()
+{
+    std::ofstream fs("/proc/self/setgroups");
+    fs << "deny";
+}
+
+void set_mapping(std::string const & filename, uid_t inside_ns, uid_t outside_ns)
+{
+    std::ofstream fs(filename);
+    fs << inside_ns << " " << outside_ns << " " << 1;
+}
+
+int my_main(int argc, char ** argv, char ** argenv)
+{
+    std::cout << "!!!!!! Running sandboxed !!!!!!" << std::endl;
+    uid_t uid = geteuid();
+    uid_t gid = getegid();
+
+    unshare(CLONE_NEWUSER);
+    setgroups_deny();
+    set_mapping("/proc/self/uid_map", 0, uid);
+    set_mapping("/proc/self/gid_map", 0, gid);
+
+    return actual_main(argc, argv, argenv);
+}
+
 }
 
 
 extern "C" {
-
-static int (*actual_main)(int, char**, char**) = nullptr;
-
-static int my_main(int argc, char ** argv, char ** argenv)
-{
-    std::cout << "!!!!!! Running sandboxed !!!!!!" << std::endl;
-    actual_main(argc, argv, argenv);
-    return 0;
-}
 
 int __libc_start_main(int (*main) (int, char**, char **),
                         int argc,
